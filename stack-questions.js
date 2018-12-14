eb9b99fe1a4c6add35a4e8b7cde9c0936f62e0b0
Arun Seehra <seehra.a@gmail.com>
2018-12-14 11:39:26 -0600

    Matching parens v1

--- stack-questions.js
+++ stack-questions.js
@@ -35,3 +35,72 @@ function testPalindromes() {
   console.log(is_palindrome('A man, a plan, a canal: Panama'));
   console.log(is_palindrome('Tauhida'));
 }
+
+/*
+Palindromes
+------------
+
+Examples:
+  Valid:
+  ()
+  (())
+  ()()
+
+  Invalid:
+  (()()
+  )(
+  )
+  (
+
+Basic alg:
+    Iterate through string
+    Count number of open, number of close
+    If #close > #open : return "invalid" immediately
+
+    If by end of string, #open = #close : "valid"
+    Else : "invalid"
+
+Stack-based alg:
+    As you iterate through the string:
+      if string[i] = '(': push i onto stack
+      if string[i] = ')':
+        if stack isn't empty:
+          stack.pop()
+        else:
+          return "invalid at {i}"
+    if stack isn't empty:
+      return "invalid at {stack.pop()}"
+    return "valid"
+*/
+
+function validParentheses(str) {
+  const openParens = new Stack();
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === '(') {
+      openParens.push(i);
+    } else if (str[i] === ')') {
+      if (openParens.top === null) {
+        return `Invalid ')' at postion ${i}`;
+      }
+
+      openParens.pop();
+    }
+  }
+
+  if (openParens.top !== null) {
+    return `No matching ')' for '(' at position ${openParens.pop()}`;
+  }
+
+  return 'Valid';
+}
+
+function testValidParens() {
+  console.log(validParentheses('()'));
+  console.log(validParentheses('())'));
+  console.log(validParentheses('(())'));
+  console.log(validParentheses('(()'));
+  console.log(validParentheses('()()'));
+}
+
+testValidParens();
